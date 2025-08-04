1471833d42d3513b5ee2103235152441688a669e
lariesther <larissafantinatti24@gmail.com>
2025-08-04 11:39:18 -0300

    programando parte 2

---
+++ pokedex/components/Header/styles.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+export const style = StyleSheet.create({
+    container:{
+        marginBottom:10,  
+    },
+    imageContainer: {
+        height: 100,
+    }
+})
